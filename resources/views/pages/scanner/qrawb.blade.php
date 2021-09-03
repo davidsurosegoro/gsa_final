62c84529154c9468b5febeef092dec051733a727
davidsurosegoro <83533489+davidsurosegoro@users.noreply.github.com>
2021-09-03 18:42:01 +0700

    Update qrawb.blade.php

--- resources/views/pages/scanner/qrawb.blade.php
+++ resources/views/pages/scanner/qrawb.blade.php
@@ -97,6 +97,66 @@
 <script type="text/javascript">  
     QrScanner.WORKER_PATH = "{{asset('assets/gsa/scanner2/qr-scanner-worker.min.js')}}"  ;
 
+
+
+    var xs      		= document.getElementById("myAudio");  
+const video         = document.getElementById('qr-video');
+const camHasCamera  = document.getElementById('cam-has-camera');
+const camList       = document.getElementById('cam-list');
+const camHasFlash   = document.getElementById('cam-has-flash');
+const flashToggle   = document.getElementById('flash-toggle');
+const flashState    = document.getElementById('flash-state');
+const camQrResult   = document.getElementById('cam-qr-result');  
+
+    // ####### Web Cam Scanning #######
+
+    const scanner = new QrScanner(video, result => setResult(camQrResult, result), error => {
+        camQrResult.textContent = error;
+        camQrResult.style.color = 'inherit';
+    });
+
+    const updateFlashAvailability = () => {
+        scanner.hasFlash().then(hasFlash => {
+            camHasFlash.textContent = hasFlash;
+            flashToggle.style.display = hasFlash ? 'inline-block' : 'none';
+        });
+    };
+
+    scanner.start().then(() => {
+        updateFlashAvailability();
+        // List cameras after the scanner started to avoid listCamera's stream and the scanner's stream being requested
+        // at the same time which can result in listCamera's unconstrained stream also being offered to the scanner.
+        // Note that we can also start the scanner after listCameras, we just have it this way around in the demo to
+        // start the scanner earlier.
+        QrScanner.listCameras(true).then(cameras => cameras.forEach(camera => {
+            const option = document.createElement('option');
+            option.value = camera.id;
+            option.text = camera.label;
+            camList.add(option);
+        }));
+    });
+
+    QrScanner.hasCamera().then(hasCamera => camHasCamera.textContent = hasCamera);
+
+    // for debugging
+    window.scanner = scanner;
+ 
+
+    camList.addEventListener('change', event => {
+        scanner.setCamera(event.target.value).then(updateFlashAvailability);
+    });
+
+    flashToggle.addEventListener('click', () => {
+        scanner.toggleFlash().then(() => flashState.textContent = scanner.isFlashOn() ? 'on' : 'off');
+    });
+
+    document.getElementById('start-button').addEventListener('click', () => {
+        scanner.start();
+    });
+
+    document.getElementById('stop-button').addEventListener('click', () => {
+        scanner.stop();
+    });
     function setResult(label, result) {
         label.textContent = result; 
         label.style.color = 'teal';
@@ -167,5 +227,5 @@
     }
 	
 </script> 
-<script src="{{asset('assets/gsa/scanner/custom-js-scanner.js')}}"></script> 
+{{-- <script src="{{asset('assets/gsa/scanner/custom-js-scanner.js')}}"></script>  --}}
 @endsection 
